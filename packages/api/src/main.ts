fd145fefd7c33b53f88ec0458695a2750a39fe19
makame <makame@mail.ru>
2020-07-09 22:22:20 +0200

    add nodemon

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,23 +1,5 @@
-import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions } from '@nestjs/microservices';
-import { AppModule } from './app.module';
-import { CORS, MS_CONFIG } from '@env/config';
-
-async function bootstrapMS() {
-  const ms = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    MS_CONFIG,
-  );
-  await ms.listenAsync();
-  console.log('Microservice is listening...');
-}
-
-async function bootstrapAPI() {
-  const app = await NestFactory.create(AppModule, { cors: CORS });
-  const port = process.env.PORT || 5000;
-  await app.listen(port);
-  console.log('Server is listening...', port);
-}
+import { bootstrapAPI } from './server';
+import { bootstrapMS } from './ms';
 
 bootstrapAPI();
 bootstrapMS();
