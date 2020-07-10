b4b09ce46da4f4af4cc80ca7311e3973c4e0c16b
makame <makame@mail.ru>
2020-07-10 16:04:48 +0200

    add tabIndex

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -4,10 +4,12 @@ import "./button.scss";
 
 export const Button: React.FC<{
   className?: string;
+  tabIndex?: number;
   onClickEnter?: () => void;
-}> = ({ children, onClickEnter, className }) => (
+}> = ({ children, onClickEnter, tabIndex, className }) => (
   <React.Fragment>
     <button
+      tabIndex={tabIndex}
       onClick={onClickEnter}
       className={classNames("button px-4", className)}
     >
